578c8363b0ffeab586ef03ebb478ddb1a4e74577
Sajib Sen <sajibsen.eee@gmail.com>
2018-02-13 06:00:00 +0000

    Added route and controller action for alice page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,6 @@ class PagesController < ApplicationController
     def home
         #render 'pages/home.html.erb'
     end
+    def alice
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 get '/pages/home', to:'pages#home', as: 'pages_home'
+get 'alice', to: 'pages#alice', as: 'pages_alice'
 
 root to: 'pages#home'
 
